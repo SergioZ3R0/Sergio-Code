757175d79866c96358bb8cdce15e5ee17a5f67cd
Sergio-CS <104630377+Sergio-CS@users.noreply.github.com>
2022-09-14 16:43:23 +0200

    Update detectchange.sh

--- detectchange.sh
+++ detectchange.sh
@@ -3,7 +3,9 @@ name=$(file ./*|grep ASCII|awk -F: '{print $1}'|head -1)
   while true; do
     mv ./"$name" ./"$name".txt
     if ["$(echo $?)" == "0"]; then
-      echo "No hay arcivos de texto en esta carpeta"
+      echo "El archivo se ha modificado con exito"
+    else
+      echo "No hay arcivos de texto en esta carpeta o ya han sido modificados"
         exit 1
     fi
 done
