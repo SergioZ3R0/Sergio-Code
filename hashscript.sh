ae57b32dc0c9fa3dc418389255e32c785f0a401b
Sergio-CS <104630377+Sergio-CS@users.noreply.github.com>
2023-05-15 17:02:50 +0200

    Update hashscript.sh

--- hashscript.sh
+++ hashscript.sh
@@ -4,9 +4,8 @@ read -p "Introduce el archivo de Output " Output
 read -p "Introduce el archivo de Hash " Hash
 read -p "Introduce el archivo de Wordlist " Wordlist
 hashtypes="0 1 2 ... 10000"
-hashCount=$(wc -l $Hash | awk '{print $1}')
+hashCount=$(wc -l "$Hash" | awk '{print $1}')
 while [ "$hashCount">="1" ]; do
-        $hashCount
         for x in {0..10000}; do
                 rm -rf /home/kali/.local/share/hashcat
                 hashcat --remove -m $x -a 0 -o $Output $Hash $Wordlist  1>/dev/null 2>/dev/null;
