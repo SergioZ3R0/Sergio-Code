030fd66eb616a4918ecf85063699ee17d49a2a60
Sergio-CS <104630377+Sergio-CS@users.noreply.github.com>
2023-05-15 15:36:40 +0200

    Update hashscript.sh

--- hashscript.sh
+++ hashscript.sh
@@ -4,11 +4,15 @@ read -p "Introduce el archivo de Output " Output
 read -p "Introduce el archivo de Hash " Hash
 read -p "Introduce el archivo de Wordlist " Wordlist
 hashtypes="0 1 2 ... 10000"
-for x in {0..10000}; do
-        rm -rf /home/kali/.local/share/hashcat
-        hashcat -m $x -a 0 -o $Output $Hash $Wordlist 1>/dev/null;
-        if [ "$(echo $?)" == "0" ]; then
-                echo "HASH CRACKED"
-                exit 5
-        fi
+hashCount=$(wc -l $Hash | awk '{print $1}')
+while [ "$hashCount">="1" ]; do
+        $hashCount
+        for x in {0..10000}; do
+                rm -rf /home/kali/.local/share/hashcat
+                hashcat --remove -m $x -a 0 -o $Output $Hash $Wordlist  1>/dev/null 2>/dev/null;
+                if [ "$(echo $?)" == "0" ]; then
+                        echo "HASH CRACKED"
+                        exit 5
+                fi
+        done
 done
