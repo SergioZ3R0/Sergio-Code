fcd2e26c9292446f85f5133a4cc27043cd979343
Sergio-CS <104630377+Sergio-CS@users.noreply.github.com>
2023-05-17 19:43:13 +0200

    Update hashscript.sh

--- hashscript.sh
+++ hashscript.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
-echo "Para que este script funcione previamente tienes que haber intalado HASHCAT sudo apt install hashcat"
+echo -e "Para que este script funcione previamente tienes que haber intalado HASHCAT sudo apt install hashcat\n"
 read -p "Introduce el archivo de Output " Output
-read -p "Introduce el archivo de Hash " Hash
+read -p "Introduce el archivo de Hash " HashFile
 read -p "Introduce el archivo de Wordlist " Wordlist
+hashnum=1
 hashtypes="0 1 2 ... 10000"
-hashCount=$(wc -l "$Hash" | awk '{print $1}')
-while [ "$hashCount">="1" ]; do
-        for x in {0..10000}; do
-                rm -rf /home/kali/.local/share/hashcat
-                hashcat --remove -m $x -a 0 -o $Output $Hash $Wordlist  1>/dev/null 2>/dev/null;
-                if [ "$(echo $?)" == "0" ]; then
-                        echo "HASH CRACKED"
-                        exit 5
-                fi
-        done
+while [ "$(wc -l "$HashFile" | awk '{print $1}')" != "0" ]; do
+        while read -r Hash; do
+                echo -e "\n\e[1;4mCracking\e[1;4;0m:\n$Hash"
+                for x in {0..10000}; do
+                        rm -rf /home/kali/.local/share/hashcat;
+                        hashcat -m $x -a 0 -o $Output $Hash $Wordlist  1>/dev/null 2>/dev/null;
+                        if [ "$(echo $?)" == "0" ]; then
+                                echo -e "\e[96;1mHASH $hashnum CRACKED\e[1;0m"
+                                hashnum=$((hashnum + 1))
+                                echo "Module used $x"
+                                echo "------------------------------------------------------------------------------------"
+                                sed -i '1d' "$HashFile"
+                                continue 2
+                        elif [ "$x" == "10000" ] && [ "(echo $?)" != "0" ]; then
+                                echo -e "\e[91;1mHash $hashnum was imposible to Crack\e[1;0m"
+                                touch UnCracked.txt
+                                echo -ne "$Hash" >> UnCracked.txt
+                                echo "------------------------------------------------------------------------------------"
+                                sed -i '1d' "$HashFile"
+                                hashnum=$((hashnum + 1))
+                                continue 2
+                        fi
+                done
+        done< "$HashFile"
 done
