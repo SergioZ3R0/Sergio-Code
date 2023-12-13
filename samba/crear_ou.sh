234f8893ca2ade878ab301cfcd804812af711351
Sergio-CS <104630377+Sergio-CS@users.noreply.github.com>
2023-12-13 19:48:36 +0100

    Update crear_ou.sh

--- samba/crear_ou.sh
+++ samba/crear_ou.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Script que crea una unitat organitzativa al domini de SambaAD amb la comanda samba-tool
-# Ús: ./crear_OU.sh
+#Author: SergioZ3R0
+# Script que crea una unidad organitzativa en el dominio de SambaAD autilizando el comando samba-tool
+# Uso: ./crear_OU.sh
 echo Este scrip se utiliza para crear unidades organizativas mediente samba-tool
 # Obtenir els dos paràmetres per entrada de l'usuari
 read -p "Itroduce el nombre de la unidad organizativa: " Name
