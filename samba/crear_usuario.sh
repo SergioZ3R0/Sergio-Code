d487a24e6dfb99ec6104fbbb2859291845983b88
SergioZ3R0 <104630377+Sergio-CS@users.noreply.github.com>
2024-01-11 11:28:58 +0100

    Update crear_usuario.sh

--- samba/crear_usuario.sh
+++ samba/crear_usuario.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Author: SergioZ3R0
 # Este script se utiliza para crar un usuario en su unidad organizativa correspondiente y lo añade a su grupo correspondiente.
-echo -n " Uso: ./crear_usuari.sh nombre nombreOU apellido displayUser Grupo\n ejemplo ./crear_usuari.sh Juan OU-Admin Garcia JGarcia gadmins"
+echo -ne " Uso: ./crear_usuari.sh nombre nombreOU apellido displayUser Grupo\n ejemplo ./crear_usuari.sh Juan OU-Admin Garcia JGarcia gadmins"
 
 #Crear usuario
 if [ $# -ne 5 ]
