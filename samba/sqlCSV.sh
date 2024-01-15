122d5b518d2b90b5d08e004fa5bfeea6d087c72d
SergioZ3R0 <104630377+Sergio-CS@users.noreply.github.com>
2024-01-15 13:51:23 +0100

    Update sqlCSV.sh

--- samba/sqlCSV.sh
+++ samba/sqlCSV.sh
@@ -10,14 +10,15 @@ SQL_QUERY="SELECT * FROM tabla_usuarios;"
 
 result=$(mysql -u "$DB_USER" -p"$DB_PASSWORD" -D "$DB_NAME" -e "$SQL_QUERY" 2>/dev/null)
 echo $result | cut -d " " -f7- | sed 's/ /,/g;'> usuarios.csv
-
+sudo sed -i 's/\([^,]*,[^,]*,[^,]*,[^,]*,[^,]*,[^,]*,\)/&\n/g' usuarios.csv
 OLDIFS=$IFS
 IFS=","
-
-while read name passwd OU surname gvname group
-do
-echo "sudo samba-tool user create "${name}" "${passwd}" --userou="OU=${OU}"   --surname="${surname}" --given-name="${gvname}""
-sudo samba-tool user create "${name}" "${passwd}" --userou="OU=${OU}"   --surname="${surname}" --given-name="${gvname}"
-#Añadir usuario al grupo correspondiente
-sudo samba-tool group addmembers "${group}" "${name}"
+while IFS= read -r line; do
+        while read name passwd OU surname gvname group; do
+                echo "sudo samba-tool user create "${name}" "${passwd}" --userou="OU=${OU}"   --surname="${surname>
+                sudo samba-tool user create "${name}" "${passwd}" --userou="OU=${OU}"   --surname="${surname}" --g>
+                #Añadir usuario al grupo correspondiente
+                sudo samba-tool group addmembers "${group}" "${name}"
+                sed -i '1d' "usuarios.csv"
+        done < usuarios.csv
 done < usuarios.csv
