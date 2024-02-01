edde37b81cdc99699e8d734d1f70a72c23f0dbd6
SergioZ3R0 <104630377+Sergio-CS@users.noreply.github.com>
2024-02-01 12:00:31 +0100

    Create exist.sh

---
+++ exist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Author: SergioZ3R0
+read -p "dime un nombre:" nombre
+file=comprobar.txt
+find -type f -name $nombre 2>/dev/null 1>comprobar.txt
+cat comprobar.txt
+if [ "$(wc -l "$file" | awk '{print $1}')" == "0" ]; then
+        echo -ne "el archivo no existe"
+        rm comprobar.txt
+else
+        echo -ne "El archivo existe"
+fi
+
