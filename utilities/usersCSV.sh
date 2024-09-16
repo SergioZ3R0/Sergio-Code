0e38cbdb28a266f7b718d5d2e08368f3489415b5
SergioZ3R0 <104630377+SergioZ3R0@users.noreply.github.com>
2024-09-16 11:01:31 +0200

    Update usersCSV.sh

--- utilities/usersCSV.sh
+++ utilities/usersCSV.sh
@@ -3,7 +3,9 @@ read -p "tell me the users file" file
 OLDIFS=$IFS
 IFS=","
  
-while read name group secgroup 
-do
-useradd ${name} -m -g ${group} -G ${secgroup} -s /bin/bash
-< $file
+while read name group secgroup; do
+ useradd ${name} -m -g ${group} -G ${secgroup} -s /bin/bash
+ if [ (echo $? == 0) ]; then
+  echo "Faltan parametros o los parametros no estan en el orden correcto\n Ej: name,group,secgroup"
+done < $file
+
