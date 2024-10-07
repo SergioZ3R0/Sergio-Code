8e5421d9208b634dda7b6a85b47ab92b33c50ff8
SergioZ3R0 <sergio.scs388@gmail.com>
2024-10-07 10:01:25 +0200

    Update

--- cyberSec/nmap/nmap_intense.py
+++ cyberSec/nmap/nmap_intense.py
@@ -3,40 +3,6 @@ import re
 import subprocess
 import os
 
-# Diccionario de puertos conocidos y sus servicios
-known_ports = {
-    20: 'FTP Data Transfer',
-    21: 'FTP Command Control',
-    22: 'SSH',
-    23: 'Telnet',
-    25: 'SMTP',
-    53: 'DNS',
-    67: 'DHCP Server',
-    68: 'DHCP Client',
-    80: 'HTTP',
-    110: 'POP3',
-    143: 'IMAP',
-    161: 'SNMP',
-    162: 'SNMP Trap',
-    194: 'IRC',
-    443: 'HTTPS',
-    445: 'Microsoft-DS',
-    587: 'SMTP Submission',
-    631: 'IPP',
-    993: 'IMAPS',
-    995: 'POP3S',
-    3306: 'MySQL',
-    5432: 'PostgreSQL',
-    6379: 'Redis',
-    8080: 'HTTP Proxy',
-    8443: 'HTTPS Proxy',
-    27017: 'MongoDB',
-    1723: 'PPTP',
-    3389: 'RDP',
-    5900: 'VNC'
-}
-
-
 def run_nmap_scan(target, output_file):
     # Run the Nmap command with additional options to evade firewalls
     subprocess.run(f"nmap {target} -p 1-65535 -T4 -Pn --max-retries 1 --min-rate 1000 -oN {output_file}", shell=True)
@@ -52,30 +18,16 @@ def parse_nmap_output(file_path):
         if "Nmap scan report for" in line:
             target = line.split("Nmap scan report for ")[1].strip()
         elif "/tcp" in line and "open" in line:
-            port = int(line.split("/")[0])
-            open_ports.append(port)
+            open_ports.append(line.strip())
 
     return target, open_ports
 
-def classify_ports(open_ports):
-    known = {}
-    redirected = []
-
-    for port in open_ports:
-        if port in known_ports:
-            known[port] = known_ports[port]
-        else:
-            redirected.append(port)
-
-    return known, redirected
-
-def save_results(target, known, redirected):
-    with open(f"scans/scan_{target}.txt", "w") as file:
+def save_results(output_file, target, open_ports):
+    with open(output_file, "w") as file:
         file.write(f"Resultados del escaneo para {target}\n")
-        file.write("Puertos conocidos abiertos:\n")
-        for port, service in known.items():
-            file.write(f"  - Puerto {port}: {service}\n")
-        file.write(f"Posibles puertos redirigidos: {redirected}\n")
+        file.write("Puertos abiertos:\n")
+        for port_info in open_ports:
+            file.write(f"  - {port_info}\n")
 
 def main():
     ip_input = input("Enter the IP address or range (e.g., 192.168.1.1 or 192.168.1.1-192.168.1.10): ")
@@ -109,32 +61,30 @@ def main():
             output_file = f"scans/scan_{current_ip}.txt"
             run_nmap_scan(current_ip, output_file)
             target, open_ports = parse_nmap_output(output_file)
-            known, redirected = classify_ports(open_ports)
 
             if target:
                 print(f"Target: {target}")
             else:
                 print("Target not found.")
 
-            if known or redirected:
-                save_results(target, known, redirected)
-                print(f"Results saved to scans/scan_{target}.txt")
+            if open_ports:
+                save_results(output_file, target, open_ports)
+                print(f"Results saved to {output_file}")
             else:
                 print("No open ports found.")
     else:
         output_file = f"scans/scan_{ip_input}.txt"
         run_nmap_scan(ip_input, output_file)
         target, open_ports = parse_nmap_output(output_file)
-        known, redirected = classify_ports(open_ports)
 
         if target:
             print(f"Target: {target}")
         else:
             print("Target not found.")
 
-        if known or redirected:
-            save_results(target, known, redirected)
-            print(f"Results saved to scans/scan_{target}.txt")
+        if open_ports:
+            save_results(output_file, target, open_ports)
+            print(f"Results saved to {output_file}")
         else:
             print("No open ports found.")
 
